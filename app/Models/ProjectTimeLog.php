c1619402c12f185f8ccc5bec304b4ac05ad98edc
bhanurathore231 <bhanurathore231@gmail.com>
2024-07-19 15:11:32 +0530

    time-logs

--- app/Models/ProjectTimeLog.php
+++ app/Models/ProjectTimeLog.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectTimeLog extends Model
 {
     use HasFactory;
-
+    protected $table = 'project_details';
     protected $fillable = ['project_id', 'employee_id', 'logtime','description','created_at','updated_at']; 
 }
