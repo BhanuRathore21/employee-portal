6fd6d0ace05fc4d7dde4c6098f1ed4a4b10803f3
bhanurathore231 <bhanurathore231@gmail.com>
2024-07-23 13:00:24 +0530

    task-list

--- resources/views/content/project_list/tasklist.blade.php
+++ resources/views/content/project_list/tasklist.blade.php
@@ -6,49 +6,61 @@
     <h4 class="py-3 mb-4">Task for Project: {{ $project->name }}</h4>
 
     <div class="card">
-        <div class="table-responsive text-nowrap m-1">
-            @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
+        <div class="table-responsive m-1">
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
             @endif
-            @if(session('usertype') === 'admin')
-            <a href="{{ route('project_list.taskscreate', ['id' => $project->id]) }}" class="btn btn-primary m-3 add-project" style="float: right;">
-                <i class="fa-solid fa-folder-plus fa-lg"></i>
-            </a>
+            @if (session('usertype') === 'admin') 
+                <a href="{{ route('project_list.taskscreate', ['id' => $project->id]) }}"
+                    class="btn btn-primary m-3 add-project" style="float: right;">
+                    <i class="fa-solid fa-folder-plus fa-lg"></i>
+                </a>
             @endif
-            <table class="table">
+             <table class="table table-striped">
                 <thead class="table-light">
                     <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Total Time</th>
-                      <th>Created</th>
-                      <th>Actions</th>
+                        <th class="text-center">ID</th>
+                        <th class="text-center">Name</th>
+                        <th class="text-center">Time Log</th>
+                        <th class="text-center">Created</th>
+                        <th class="text-center">Status</th>
+                        <th class="text-center">Actions</th>
                     </tr>
-                  </thead>
+                </thead>
                 <tbody>
                     @foreach ($tasks as $task)
-                    @if(session('user_id') == $task->user_id || session('usertype') == 'admin')
-                        <tr>
-                            <td><strong>{{ $task->id }}</strong></td>
-                            <td><i class="mdi mdi-wallet-travel mdi-20px text-danger me-3"></i><span class="fw-medium"><strong>{{ $task->name }}</strong></span></td>
-                            <td><strong>{{ $task->total_time }}</strong></td>
-                            <td><strong>{{ $task->created_at }}</strong></td>
-                            <td>
-                                @if ($task->active == 1)
-                                    <span class="badge bg-label-success rounded-pill">Done</span>
-                                @elseif($task->active == 2)
-                                    <span class="badge bg-label-secondary rounded-pill">Pending</span>
-                                @elseif($task->active == 3)
-                                    <span class="badge bg-label-warning rounded-pill">Bug</span>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="#">Edit</a> | <a href="#">Delete</a>
-                            </td>
-                        </tr>
-                    @endif
+                        @if (session('user_id') == $task->user_id || session('usertype') == 'admin')
+                            <tr>
+                                <td class="text-center"><strong>{{ $task->id }}</strong></td>
+                                <td>
+                                    <div class="d-flex align-items-center">
+                                        <i class="mdi mdi-progress-clock mdi-24px text-info me-3"></i>
+                                        <div>
+                                            <span class="fw-medium"><strong>{{ $task->name }}</strong></span>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="text-center"><strong>{{ $task->total_time }}</strong></td>
+                                <td class="text-center"><strong>{{ $task->created_at->format('j F Y h:i a') }}</strong></td>
+                                <td class="text-center">
+                                    @if ($task->status == 1)
+                                        <span class="badge bg-success"><i
+                                                class="mdi mdi-checkbox-marked-circle-outline"></i> Done</span>
+                                    @elseif($task->status == 2)
+                                        <span class="badge bg-warning text-dark"><i class="mdi mdi-clock-alert-outline"></i>
+                                            Pending</span>
+                                    @elseif($task->status == 3)
+                                        <span class="badge bg-danger"><i class="mdi mdi-bug-outline"></i> Bug</span>
+                                    @endif
+                                </td>
+                                <td class="text-center">
+                                    <a href="#"><i class="fas fa-edit fa-lg"></i></a>
+                                    <a href="#"><i class="fas fa-trash fa-lg"></i></a>
+                                </td>
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
             </table>
